aeaf2c41341dcf1c9aae3468d63588f738b10aeb
Harsh Mittal <harshmittal1750@gmail.com>
2024-06-16 02:22:22 +0530

    tsc fix

--- pages/products/Products.tsx
+++ pages/products/Products.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { ProductsData } from "./ProductsData";
 import Image from "next/image";
 import {
   Card,
@@ -11,6 +10,27 @@ import {
 } from "@/components/ui/card";
 import { Badge } from "@/components/ui/badge";
 import Link from "next/link";
+
+export const ProductsData = [
+  {
+    slug: "product-1",
+    name: "Product 1",
+    image: "https://via.placeholder.com/150",
+    features: 10,
+  },
+  {
+    slug: "product-2",
+    name: "Product 2",
+    image: "https://via.placeholder.com/150",
+    features: 20,
+  },
+  {
+    slug: "product-3",
+    name: "Product 3",
+    image: "https://via.placeholder.com/150",
+    features: 30,
+  },
+];
 
 function Products() {
   return (
--- utils/getAttestation.ts
+++ utils/getAttestation.ts
@@ -2,10 +2,10 @@ import { EAS } from "@ethereum-attestation-service/eas-sdk";
 import { ethers } from "ethers";
 
 export const EASContractAddress = "0xC2679fBD37d54388Ce493F1DB75320D236e1815e"; // Sepolia v0.26
-const provider: any = ethers.providers.getDefaultProvider("sepolia");
+// const provider: any = ethers.providers.getDefaultProvider("sepolia");
 
 const eas = new EAS(EASContractAddress);
-eas.connect(provider);
+// eas.connect(provider);
 
 export const getAttestation = async (uid: string) => {
   const attestation = await eas.getAttestation(uid);
